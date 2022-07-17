772639e742b5325c7a8f9eb6fb565781e3bf5641
Robert Vitoriano <robertvitoriano@gmail.com>
2022-07-17 11:54:13 -0300

    get piece position from redux

--- src/components/Piece/Piece.jsx
+++ src/components/Piece/Piece.jsx
@@ -5,14 +5,9 @@ import { playerWillMoveAction, setPossibleMovesAction, setPieceBeingMovedAction 
 
 export const Piece = ({ rowPosition, columnPosition }) => {
 
-  const [pieceSettings, setPieceSettings] = useState({
-    piece: '',
-    playerSide: '',
-    row: { letter: '', number: 0 },
-    column: 0,
-    image: '',
-  });
+
   const [isHover, setIsHover] = useState(false)
+  const [currentPosition, setCurrentPosition] = useState({})
   const selectedPiece = useSelector((state) => state.movementState.selectedPiece)
   const possibleMoves = useSelector((state) => state.movementState.possibleMoves)
   const pieceBeingMoved = useSelector((state) => state.movementState.pieceBeingMoved)
@@ -23,102 +18,17 @@ export const Piece = ({ rowPosition, columnPosition }) => {
   const wasPossibleMoveSelected = selectedPossibleMove.row.number && selectedPossibleMove.column
   const shouldErase = pieceBeingMoved.row.number === rowPosition.number && pieceBeingMoved.column === columnPosition && wasPossibleMoveSelected
   const rows = useSelector((state) => state.movementState.rows)
+  const positions = useSelector((state) => state.movementState.positions)
+
+  useEffect(()=>{
+    if(positions && positions.length){
+      const position = positions.filter((position)=>position.column === columnPosition && rowPosition.number === position.row.number)[0];
+      setCurrentPosition(position)
+    }
+  },[positions, rowPosition, columnPosition])
+  
   const dispatch = useDispatch()
 
-  useEffect(() => {
-    setPiecesInitialPositions(rowPosition, columnPosition);
-  }, [])
-
-  const setPiecesInitialPositions = (rowPosition, columnPosition) => {
-    const pieceImages = {
-      blackBishop: 'https://robertvitoriano-images.s3.amazonaws.com/pieces/black_bishop.png',
-      blackKing: 'https://robertvitoriano-images.s3.amazonaws.com/pieces/black_king.png',
-      blackHorse: 'https://robertvitoriano-images.s3.amazonaws.com/pieces/black_horse.png',
-      blackPawn: 'https://robertvitoriano-images.s3.amazonaws.com/pieces/black_pawn.png',
-      blackQueen: 'https://robertvitoriano-images.s3.amazonaws.com/pieces/black_queen.png',
-      blackRook: 'https://robertvitoriano-images.s3.amazonaws.com/pieces/black_rook.png',
-      whiteBishop: 'https://robertvitoriano-images.s3.amazonaws.com/pieces/white_bishop.png',
-      whiteKing: 'https://robertvitoriano-images.s3.amazonaws.com/pieces/white_king.png',
-      whiteHorse: 'https://robertvitoriano-images.s3.amazonaws.com/pieces/white_horse.png',
-      whitePawn: 'https://robertvitoriano-images.s3.amazonaws.com/pieces/white_pawn.png',
-      whiteQueen: 'https://robertvitoriano-images.s3.amazonaws.com/pieces/white_queen.png',
-      whiteRook: 'https://robertvitoriano-images.s3.amazonaws.com/pieces/white_rook.png',
-    }
-
-    if (rowPosition.letter === 'h' && columnPosition === 1) {
-      return setPieceSettings({ image: pieceImages.whiteRook, column: columnPosition, row: rowPosition, playerSide: 'white', piece: 'rook', instance: 1 });
-    }
-    //WHITE ROOK
-    if (rowPosition.letter === 'h' && columnPosition === 8) {
-      return setPieceSettings({ image: pieceImages.whiteRook, column: columnPosition, row: rowPosition, playerSide: 'white', piece: 'rook', instance: 2 });
-    }
-    //WHITE HORSE
-    if (rowPosition.letter === 'h' && columnPosition === 2) {
-      return setPieceSettings({ image: pieceImages.whiteHorse, column: columnPosition, row: rowPosition, playerSide: 'white', piece: 'horse', instance: 1 });
-    }
-    //WHITE HORSE
-    if (rowPosition.letter === 'h' && columnPosition === 7) {
-      return setPieceSettings({ image: pieceImages.whiteHorse, column: columnPosition, row: rowPosition, playerSide: 'white', piece: 'horse', instance: 2 });
-    }
-    //BLACK ROOK
-    if (rowPosition.letter === 'a' && columnPosition === 1) {
-      return setPieceSettings({ image: pieceImages.blackRook, column: columnPosition, row: rowPosition, playerSide: 'black', piece: 'rook', instance: 1 });
-    }
-    if (rowPosition.letter === 'a' && columnPosition === 8) {
-      return setPieceSettings({ image: pieceImages.blackRook, column: columnPosition, row: rowPosition, playerSide: 'black', piece: 'rook', instance: 2 });
-    }
-    //BLACK HORSE
-    if (rowPosition.letter === 'a' && columnPosition === 2) {
-      return setPieceSettings({ image: pieceImages.blackHorse, column: columnPosition, row: rowPosition, playerSide: 'black', piece: 'horse', instance: 1 });
-    }
-    if (rowPosition.letter === 'a' && columnPosition === 7) {
-      return setPieceSettings({ image: pieceImages.blackHorse, column: columnPosition, row: rowPosition, playerSide: 'black', piece: 'horse', instance: 2 });
-    }
-    //WHITE BISHOP
-
-    if (rowPosition.letter === 'h' && columnPosition === 3) {
-      return setPieceSettings({ image: pieceImages.whiteBishop, column: columnPosition, row: rowPosition, playerSide: 'white', piece: 'bishop', instance: 1 });
-    }
-
-    if (rowPosition.letter === 'h' && columnPosition === 6) {
-      return setPieceSettings({ image: pieceImages.whiteBishop, column: columnPosition, row: rowPosition, playerSide: 'white', piece: 'bishop', instance: 2 });
-    }
-
-    //FIRST BLACK BISHOP
-    if (rowPosition.letter === 'a' && columnPosition === 3) {
-
-      return setPieceSettings({ image: pieceImages.blackBishop, column: columnPosition, row: rowPosition, playerSide: 'black', piece: 'bishop', instance: 1 });
-    }
-    //SECOND BLACK BISHOP
-    if (rowPosition.letter === 'a' && columnPosition === 6) {
-      return setPieceSettings({ image: pieceImages.blackBishop, column: columnPosition, row: rowPosition, playerSide: 'black', piece: 'bishop', instance: 2 });
-    }
-    //WHITE QUEEN
-    if (rowPosition.letter === 'h' && columnPosition === 4) {
-      return setPieceSettings({ image: pieceImages.whiteQueen, column: columnPosition, row: rowPosition, playerSide: 'white', piece: 'queen' });
-    }
-    //BLACK QUEEN
-    if (rowPosition.letter === 'a' && columnPosition === 4) {
-      return setPieceSettings({ image: pieceImages.blackQueen, column: columnPosition, row: rowPosition, playerSide: 'black', piece: 'queen' });
-    }
-    //WHITE KING
-    if (rowPosition.letter === 'h' && columnPosition === 5) {
-      return setPieceSettings({ image: pieceImages.whiteKing, column: columnPosition, row: rowPosition, playerSide: 'white', piece: 'king' });
-    }
-
-    //BLACK KING
-    if (rowPosition.letter === 'a' && columnPosition === 5) {
-      return setPieceSettings({ image: pieceImages.blackKing, column: columnPosition, row: rowPosition, playerSide: 'black', piece: 'king' });
-    }
-
-    //WHITE PAWN
-    if (rowPosition.letter === 'g') return setPieceSettings({ image: pieceImages.whitePawn, column: columnPosition, row: rowPosition, playerSide: 'white', piece: 'pawn' });
-    //BLACK PAWN  
-    if (rowPosition.letter === 'b') return setPieceSettings({ image: pieceImages.blackPawn, column: columnPosition, row: rowPosition, playerSide: 'black', piece: 'pawn' });
-
-    setPieceSettings({ image: '', column: columnPosition, row: rowPosition, playerSide: '', piece: '' });
-
-  }
   const handleSelection = (data) => {
     const { row, column, image, playerSide, piece } = data;
     if (row.number === selectedPiece.row.number && column === selectedPiece.column) {
@@ -217,9 +127,9 @@ export const Piece = ({ rowPosition, columnPosition }) => {
       onMouseLeave={() => setIsHover(false)}
       isHover={isHover}
       isSelected={!!selectedPiece && selectedPiece.row.number === rowPosition.number && selectedPiece.column === columnPosition}
-      onClick={() => handleSelection(pieceSettings)}
+      onClick={() => handleSelection(currentPosition)}
     >
-      {shouldMove ? <img src={pieceBeingMoved.image} width="60px" /> : shouldErase ? '' : <img src={pieceSettings.image} width="60px" />}
+      {shouldMove ? <img src={pieceBeingMoved.image} width="60px" /> : shouldErase ? '' : <img src={currentPosition && currentPosition.image} width="60px" />}
     </Container>
   )
 }
